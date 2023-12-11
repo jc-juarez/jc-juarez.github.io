3c01edc1b2c62df4088b50b521ad9451ef7a5bae
jc-juarez <goarp_jc@hotmail.com>
2023-12-11 02:42:10 -0800

    Updated pages

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional JavaScript for interactive elements
+// Example: Smooth scrolling for links with hashes
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
